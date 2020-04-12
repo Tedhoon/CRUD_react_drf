b2289844b89ec679377b205c1b4b25a15c353a0c
Tedhoon <gt0305@likelion.org>
2020-04-13 01:27:49 +0900

    [add] getallPost로 데이터 모두 가져오기, props이용해서 rendering해주기

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import api from './api';
-// import PostView from './Components/PostView';
+import PostView from './Components/PostView';
 
 class App extends React.Component { 
 
@@ -10,17 +10,30 @@ class App extends React.Component {
     this.state = {
       title:'',
       content:'',
-      results:[]
+      allPost:[]
     }
   }
 
-  // componentDidMount() {
-  //   this.getPosts()
-  // }
+  componentDidMount() {
+    this.getPosts()
+  }
+  
+  async getPosts() {
+    const allPost = await api.getAllPosts()
+    console.log(allPost)
+    this.setState({allPost: allPost.data})
+    console.log(allPost.data)
+    
+  }
   // getPosts() {
-  //   const _results = "hello" // getApi 함수 실행
-  //   this.setState({results: _results})
+  //   const allPost = api.getAllPosts()
+  //   console.log(allPost)
+  //   this.setState({allPost: allPost.data})
+    
   // }
+  // 여기도!!!!!!! 비동기 처리를 안해주면 pending상태이기 때문에 allPost는 undefined로 나타날 것임!
+  // 자세히 설명하자만 await을 안해주면(안기다려주면) 요청하는데 걸리는 시간을 기다리지 않고 다음 줄로 넘어가기 때문에 await을 넣어줘야하는 것!
+
 
   // handlingChange = (event) => {
   //   this.setState({title:event.target.value})
@@ -40,8 +53,8 @@ class App extends React.Component {
   handlingSubmit = async (event) => {
     event.preventDefault()
     let result = await api.createPost({title:this.state.title, content:this.state.content})
-     //해당 이벤트의 고유 기능을 막아줌, 여기서는 submit의 새로고침
-    console.log("성공", result)
+    //해당 이벤트의 고유 기능을 막아줌, 여기서는 submit의 새로고침
+    console.log("성공", result.data)
     // 모델에 있는 title, content에 json형식으로 전달
     // await이 없으면 pending, await을 넣기 위해 함수의 시작에 async필요
   }
@@ -65,6 +78,12 @@ class App extends React.Component {
           <br />
           <button type="submit">제출하기</button>
         </form>
+        {
+          this.state.allPost.map((post)=>
+          <PostView title={post.title} content={post.content}/>
+          )
+        }
+        
       </div>
     )
   }
--- reactfrontend/src/Components/PostView.js
+++ reactfrontend/src/Components/PostView.js
@@ -11,8 +11,8 @@ class PostView extends Component {
         const {title, content} = dummy_prop //비구조화 할당
         return (
             <div>
-                <h3>{title}</h3>
-                <p>{content}</p>
+                <h3>{this.props.title}</h3>
+                <p>{this.props.content}</p>
             </div>
         );
     }
@@ -25,26 +25,26 @@ export default PostView;
 
 // 오브젝트의 비구조화 할당법!!
 
-const animals = {
-    cat: "CAT",
-    dog: "DOG",
-    tiger: "TIGER"
-  };
-  const cat = animals.cat;
-  const dog = animals.dog;
-  const tiger = animals.tiger;
-  console.log(cat); // CAT
-  console.log(dog); // DOG
-  console.log(tiger); // TIGER
+// const animals = {
+//     cat: "CAT",
+//     dog: "DOG",
+//     tiger: "TIGER"
+//   };
+//   const cat = animals.cat;
+//   const dog = animals.dog;
+//   const tiger = animals.tiger;
+//   console.log(cat); // CAT
+//   console.log(dog); // DOG
+//   console.log(tiger); // TIGER
 
-  //또는
-  // 밑에처럼 작성해도 비구조화 할당을 수행하며,
-  // 변수의 이름과 같은 key에 있는 value가 담깁니당
-  const { cat, dog, tiger } = {
-    cat: "CAT",
-    dog: "DOG",
-    tiger: "TIGER"
-  };
-  console.log(cat); // CAT
-  console.log(dog); // DOG
-  console.log(tiger); // TIGER
+//   //또는
+//   // 밑에처럼 작성해도 비구조화 할당을 수행하며,
+//   // 변수의 이름과 같은 key에 있는 value가 담깁니당
+//   const { cat, dog, tiger } = {
+//     cat: "CAT",
+//     dog: "DOG",
+//     tiger: "TIGER"
+//   };
+//   console.log(cat); // CAT
+//   console.log(dog); // DOG
+//   console.log(tiger); // TIGER
